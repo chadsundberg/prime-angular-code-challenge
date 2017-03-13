9dc0bae5ba74370306da89cc321b961e2e8921cd
Chad Sundberg <chadsundberg@gmail.com>
2017-03-13 15:15:54 -0500

    Luke walkthrough improvement part 2

--- server/public/scripts/controllers/hero-list.controller.js
+++ server/public/scripts/controllers/hero-list.controller.js
@@ -1,18 +1,26 @@
 heroApp.controller('HeroListController', ['$http', function($http){
     console.log('Hero List Controller loaded');
     var self = this;
-      self.heroList = [];
-      self.newHero = {};
+    self.heroList = [];
 
-      getHeroes();
+    $http({
+      method: 'GET',
+      url: '/heroes'
+    }).then(function(response){
+      console.log(response.data);
+      self.heroList = response.data;
+    });
+
+
+      // getHeroes();
 
   // get all employee data
-  function getHeroes() {
-    $http.get('/heroes').then(function(response) {
-      self.heroList = response.data;
-      getHeroes();
-    });
-  }
+  // function getHeroes() {
+  //   $http.get('/heroes').then(function(response) {
+  //     self.heroList = response.data;
+  //     getHeroes();
+  //   });
+  // }
 
 
 }]);
